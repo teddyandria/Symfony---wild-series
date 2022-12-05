20a56de4484a27fff08c9ffe138092a94b6350e5
teddyandria <teddy.andriamam@gmail.com>
2022-12-05 15:54:35 +0100

    symfony12

--- src/DataFixtures/SeasonFixtures.php
+++ src/DataFixtures/SeasonFixtures.php
@@ -2,24 +2,26 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\Season;
+use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
-use Doctrine\Persistence\ObjectManager;
 
 class SeasonFixtures extends Fixture implements DependentFixtureInterface
 {
 
     public function load(ObjectManager $manager): void
     {
-        $categoryKey = 0;
+        $faker = Factory::create();
+
         foreach (CategoryFixtures::CATEGORIES as $categoryKey => $categoryName) {
             for ($i = 0; $i < 3; $i++) { //1 boucle pour créer 5 séries d'un coup
-                for ($j = 0; $j < 5; $j++) { // 5 saisons par série
+                for ($j = 0; $j <= 5; $j++) { // 5 saisons par série
                     $season = new Season();
                     $season->setNumber($j);
-                    $season->setYear(2015);
-                    $season->setDescription('La saison est cool');
+                    $season->setYear($faker->year());
+                    $season->setDescription($faker->paragraphs(3, true));
                     $program = $this->getReference('category_' . $categoryKey . '_program_' . $i);
                     $this->addReference('category_' . $categoryKey . '_program_' . $i . '_season_' . $j, $season);
                     $season->setProgram($program);
