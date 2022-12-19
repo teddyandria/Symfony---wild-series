0e2987d413bed50822082bc1cc7c2ad8c2fc47c1
teddyandria <teddy.andriamam@gmail.com>
2022-12-19 10:11:24 +0100

    symfony19

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -16,17 +16,23 @@ class ProgramType extends AbstractType
     {
 
         $builder
-            ->add('actors', EntityType::class, [
-                'class' => Actor::class,
-                'choice_label' => 'name',
-                'multiple' => true,
-                'expanded' => false,
-            ])
 
             ->add('title', TextType::class)
             ->add('synopsis', TextType::class)
             ->add('poster')
-            ->add('category', null, ['choice_label' => 'name']);
+            ->add('category', null, ['choice_label' => 'name'])
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'attr' => ['class' => 'form-select my-2 '],
+                'help' => 'Déroulez le menu et sélectionnez les acteurs jouant dans le programme ...',
+                'help_attr' => [
+                    'class' => 'text-secondary fw-light ',
+                ],
+                'by_reference' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
